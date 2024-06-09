b484cffa926e093a8a8bfd373a86d244923bac12
kimu7466 <kimu7466@gmail.com>
2024-06-09 07:19:12 +0530

    #

--- manage_institute/forms.py
+++ manage_institute/forms.py
@@ -20,22 +20,3 @@ class StudentForm(forms.ModelForm):
 
 class ForgotPasswordForm(forms.Form):
     email = forms.EmailField(label='Email', max_length=254)
-
-class ResetPasswordForm(forms.Form):
-    otp = forms.CharField(label='OTP', max_length=6)
-    new_password = forms.CharField(label='New Password', widget=forms.PasswordInput)
-
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
-        super(ResetPasswordForm, self).__init__(*args, **kwargs)
-
-    def clean_otp(self):
-        otp = self.cleaned_data['otp']
-        if self.user.otp != otp:
-            raise forms.ValidationError("Invalid OTP")
-        return otp
-
-    def save(self):
-        new_password = self.cleaned_data['new_password']
-        self.user.set_password(new_password)
-        self.user.save()
--- manage_institute/models.py
+++ manage_institute/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-# from.models import baseModel
-# from django import Department
-
 
 class Department(models.Model):
     name = models.CharField(max_length=100, blank=True)
@@ -46,57 +43,3 @@ class Book(models.Model):
     def __str__(self):
         return self.name
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class customersModel(baseModel):
-#     customer_id = models.CharField(primary_key=True,max_length=255, blank=True)
-#     first_name = models.CharField(max_length=255, blank=True)
-#     last_name = models.CharField(max_length=255, blank=True)
-#     email = models.EmailField(max_length=255, blank=False, null=False, unique=True)
-#     mobile = models.CharField(max_length=255, blank=True)
-#     password = models.CharField(max_length=255)
-#     is_activate = models.BooleanField(default=False)
-#     is_added_address = models.BooleanField(default=False)
-#     otp = models.CharField(max_length=10, default="111111")
-
-#     def __str__(self):
-#         return f"{self.customer_id} - {self.first_name} {self.last_name}"
-
-#     def save(self, *args, **kwargs):
-#         if not self.customer_id:
-#             self.customer_id = self.generate_customer_id()
-#         return super(customersModel, self).save(*args, **kwargs)
-    
-#     def generate_customer_id(self):
-#         last_customer = customersModel.objects.order_by('-customer_id').first()
-#         if last_customer:
-#             last_id = int(last_customer.customer_id[3:])  # Extracting the numeric part
-#             new_id = last_id + 1
-#         else:
-#             new_id = 1
-#         return 'CUM{:04d}'.format(new_id)
-    
-
-# class customerAddressModel(baseModel):
-    # customer_id = models.ForeignKey(customersModel, on_delete=models.CASCADE)
-    # street_address = models.CharField(max_length=255)
-    # city = models.CharField(max_length=255)
-    # pincode = models.CharField(max_length=255)
-    # state = models.CharField(max_length=255)
